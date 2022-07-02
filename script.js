7f46df2ffbc6244d2ddf16c0b298eff284f4e2bf
Aiden Lee <75740207+snaapsh0t12@users.noreply.github.com>
2022-07-02 00:28:30 -0500

    this is a lot of stuff i'm just going to say pretty much rewriting the site

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
 const showClass = "show";
 
+const faders = document.querySelectorAll('.fade-in');
+
 // Only make it work on not very small screens???
 // if (this.matchMedia("(min-width: 768px)").matches)
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+
+  })
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
 
 $(window).on("load resize", function() {
     $dropdown.hover(
