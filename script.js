a2471213ed6e1467f27ed02f24cd366df9e66603
Aiden Lee <75740207+snaapsh0t12@users.noreply.github.com>
2022-07-11 03:34:25 -0500

    well I added this stupid to top button

--- script.js
+++ script.js
@@ -53,5 +53,44 @@ $(window).on("load resize", function() {
       }
       
     );
-  
-});
+  });
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    mybutton.style.display = "block";
+    // mybutton.style.opacity = "1";
+    mybutton.classList.add('appear');
+    // mybutton.target.classList.add('appear');
+    // mybutton.setAttribute("style","opacity:1.0; -moz-opacity:0.5; filter:alpha(opacity=50)");
+  } else {
+    // mybutton.style.opacity = "0";
+    mybutton.style.display = "none";
+    mybutton.classList.remove('appear')
+    
+    
+    // mybutton.target.classList.removeClass('appear');
+    // mybutton.setAttribute("style","opacity:1.0; -moz-opacity:0.5; filter:alpha(opacity=50)");
+  }
+}
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
